1a1a23fe3851fa4f7693408b82429a39ae06b189
Filipe Cristóvão <filipe.md.cristovao@gmail.com>
2013-10-17 22:32:39 +0200

    Add build configurations for sbt and travis

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "svstm"
+
+version := "0.0"
+
+scalaVersion := "2.10.2"
+
+//libraryDependencies += ("org.scala-stm" %% "scala-stm" % "0.7")
---
+++ project/Build.scala
@@ -0,0 +1,15 @@
+import sbt._
+
+case class Remote(uri: String, branch: String)
+
+object Config {
+  val scalaSTM = Remote("https://github.com/fcristovao/scala-stm.git", "master")
+}
+
+object Projects {
+  lazy val scalaSTM = RootProject(uri("%s#%s".format(Config.scalaSTM.uri, Config.scalaSTM.branch)))
+}
+
+object SVSTMBuild extends Build {
+  lazy val svstm = Project("svstm", file(".")) dependsOn(Projects.scalaSTM)
+}
