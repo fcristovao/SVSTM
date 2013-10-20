687013625416bec2bd7f694b008f159aaae7ef0d
Filipe Cristóvão <filipe.md.cristovao@gmail.com>
2013-10-20 15:32:19 +0200

    VBox now descends from StubRef

--- src/main/scala/jvstm/vbox/VBox.scala
+++ src/main/scala/jvstm/vbox/VBox.scala
@@ -1,29 +1,9 @@
 package jvstm.vbox
 
-import scala.concurrent.stm.Ref
-import scala.concurrent.stm.InTxn
-import scala.concurrent.stm.Ref.View
+import scala.concurrent.stm.stubs.StubRef
 
-import jvstm.exceptions.NotImplemented
-
-class VBox[A](val body: VBoxBody[A]) extends Ref[A] {
+class VBox[A](val body: VBoxBody[A]) extends StubRef[A] {
 	
 	def this(v0: A) = this(new VBoxBody[A](v0))
 	
-	//from SourceLike[A]:
-	def get(implicit txn: InTxn): A = throw NotImplemented
-  def getWith[Z](f: (A) => Z)(implicit txn: InTxn): Z = throw NotImplemented
-  def relaxedGet(equiv: (A, A) => Boolean)(implicit txn: InTxn): A = throw NotImplemented 
-  
-  //from SinkLike[A]
-  def set(v: A)(implicit txn: InTxn) = throw NotImplemented
-  def trySet(v: A)(implicit txn: InTxn): Boolean = throw NotImplemented
-
-  //from RefLike[A]
-  def swap(v: A)(implicit txn: InTxn): A = throw NotImplemented
-  def transform(f: A => A)(implicit txn: InTxn) = throw NotImplemented
-  def transformIfDefined(pf: PartialFunction[A,A])(implicit txn: InTxn): Boolean = throw NotImplemented
-  
-  //from Ref[A]
-  def single: View[A] = throw NotImplemented
 }
